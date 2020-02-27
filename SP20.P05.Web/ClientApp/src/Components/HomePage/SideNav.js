4d4b14405e1c4fb2e4b1224810470cc10e91fba1
Darryl Munsch <darrylmunsch@gmail.com>
2020-02-26 23:19:59 -0600

    SideNav WIP

--- SP20.P05.Web/ClientApp/src/Components/HomePage/SideNav.js
+++ SP20.P05.Web/ClientApp/src/Components/HomePage/SideNav.js
@@ -35,66 +35,67 @@ class SideNavbar extends Component {
         const { isOpen } = this.state;
     
         return (
-          <div style={{ width: 200 }}>
+            <div style={{ width: 200 }}>
             <Toggle onChange={this.handleToggle} checked={isOpen} />
             <hr />
             <Sidenav
               expanded={isOpen}
-              defaultOpenKeys={['3', '4']}
               activeKey={this.state.activeKey}
               onSelect={this.handleSelect}
             >
               <Sidenav.Body>
                 <Nav>
-                  <Dropdown 
-                    placement="rightStart" 
-                    title="Browse Fields" 
-                    eventKey="1" 
-                    icon={<Icon icon="tree" />}
-                  >
-                    Browse Fields
-                    <Dropdown.Item eventKey="1-1">Blueberries</Dropdown.Item>
-                    <Dropdown.Item eventKey="1-2">Blackberries</Dropdown.Item>
-                    <Dropdown.Item eventKey="1-3">Potatoes</Dropdown.Item>
-                    <Dropdown.Item eventKey="1-4">Corn</Dropdown.Item>
-                  </Dropdown>
-                  <Nav.Item eventKey="2" icon={<Icon icon="shopping-basket" />}>
-                    Basket
-                  </Nav.Item>
-                  <Nav.Item eventKey="3" icon = {<Icon icon="order-form" />}>
-                    Order Ticket
-                  </Nav.Item>
-                  <Dropdown
-                    placement="rightStart"
-                    eventKey="4"
-                    title="Advanced"
-                    icon={<Icon icon="magic" />}
-                  >
-                    <Dropdown.Item eventKey="4-1">Geo</Dropdown.Item>
-                    <Dropdown.Item eventKey="4-2">Devices</Dropdown.Item>
-                    <Dropdown.Item eventKey="4-3">Loyalty</Dropdown.Item>
-                    <Dropdown.Item eventKey="4-4">Visit Depth</Dropdown.Item>
-                  </Dropdown>
-                  <Dropdown
-                    placement="rightStart"
-                    eventKey="5"
-                    title="Settings"
-                    icon={<Icon icon="gear-circle" />}
-                  >
-                    <Dropdown.Item eventKey="5-1">Applications</Dropdown.Item>
-                    <Dropdown.Item eventKey="5-2">Channels</Dropdown.Item>
-                    <Dropdown.Item eventKey="5-3">Versions</Dropdown.Item>
-                    <Dropdown.Menu eventKey="5-5" title="Custom Action">
-                      <Dropdown.Item eventKey="5-5-1">Action Name</Dropdown.Item>
-                      <Dropdown.Item eventKey="5-5-2">Action Params</Dropdown.Item>
-                    </Dropdown.Menu>
-                  </Dropdown>
-                </Nav>
-              </Sidenav.Body>
-            </Sidenav>
-          </div>
+                    <Nav.Item eventKey="1" icon={<Icon icon="home" />}>
+                        Home
+                    </Nav.Item>
+                    <Nav.Item eventKey="2" icon={<Icon icon="shopping-basket" />}>
+                        Baskets
+                    </Nav.Item>
+                    <Nav.Item eventKey="3" icon = {<Icon icon="order-form" />}>
+                        Order Ticket
+                    </Nav.Item>
+                    <Dropdown 
+                        placement="rightStart" 
+                        title="Browse Fields" 
+                        eventKey="4" 
+                        icon={<Icon icon="tree" />}
+                    >
+                        <Dropdown.Item eventKey="4-1">Blueberries</Dropdown.Item>
+                        <Dropdown.Item eventKey="4-2">Blackberries</Dropdown.Item>
+                        <Dropdown.Item eventKey="4-3">Potatoes</Dropdown.Item>
+                        <Dropdown.Item eventKey="4-4">Corn</Dropdown.Item>
+                    </Dropdown>                 
+                    <Dropdown
+                        placement="rightStart"
+                        eventKey="5"
+                        title="Advanced"
+                        icon={<Icon icon="magic" />}
+                    >
+                        <Dropdown.Item eventKey="5-1">Geo</Dropdown.Item>
+                        <Dropdown.Item eventKey="5-2">Devices</Dropdown.Item>
+                        <Dropdown.Item eventKey="5-3">Loyalty</Dropdown.Item>
+                        <Dropdown.Item eventKey="5-4">Visit Depth</Dropdown.Item>
+                    </Dropdown>
+                    <Dropdown
+                        placement="rightStart"
+                        eventKey="6"
+                        title="Settings"
+                        icon={<Icon icon="gear-circle" />}
+                    >
+                        <Dropdown.Item eventKey="6-1">Applications</Dropdown.Item>
+                        <Dropdown.Item eventKey="6-2">Channels</Dropdown.Item>
+                        <Dropdown.Item eventKey="6-3">Versions</Dropdown.Item>
+                        <Dropdown.Menu eventKey="6-5" title="Custom Action">
+                        <Dropdown.Item eventKey="6-5-1">Action Name</Dropdown.Item>
+                        <Dropdown.Item eventKey="6-5-2">Action Params</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                    </Nav>
+                    </Sidenav.Body>
+                </Sidenav>
+            </div>
         );
-      }
+    }
 }
 
 export default SideNavbar;
