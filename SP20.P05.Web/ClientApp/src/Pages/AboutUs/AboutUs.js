9b085329c81884b1a5b6be13155e19b67598f532
kade2711 <kade.day@selu.edu>
2020-02-29 11:34:27 -0600

    -Added AboutUs page elements
    -Added AboutUs page CSS
    -Added AboutUs abstract information w/ texts(WIP)

--- SP20.P05.Web/ClientApp/src/Pages/AboutUs/AboutUs.js
+++ SP20.P05.Web/ClientApp/src/Pages/AboutUs/AboutUs.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
+import SideNav from '../../Components/SideNav/SideNav';
+
+
+import './AboutUs.css';
+
 
 export class AboutUs extends Component {
     render() {
         return (
             <div>
-                <h1>AboutUs</h1>
+              <SideNav />
+               
+                <div className="background-img">
+                    <img src={require("../../Resources/orchard.jpg")}  />
+                <h2><span>Our farms are closest to home...</span></h2>
+                
+                 <h3><span>Info about farms</span></h3>
+                 <h4><span>Some more info about the place</span></h4>
+                </div>                   
+                
             </div>
         )
     }
