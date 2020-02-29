f6137f4bf7c3088be37d31bf024bbe283aef99dd
kade2711 <kade.day@selu.edu>
2020-02-29 12:35:58 -0600

    -haha forget everything i did in the last commit ha ha

--- SP20.P05.Web/ClientApp/src/Pages/AboutUs/AboutUs.js
+++ SP20.P05.Web/ClientApp/src/Pages/AboutUs/AboutUs.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react'
 import SideNav from '../../Components/SideNav/SideNav';
+import Card from 'react-bootstrap/Card';
+import CardColumns from "react-bootstrap/CardColumns";
+
+// Style sheets
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 import './AboutUs.css';
@@ -10,7 +15,42 @@ export class AboutUs extends Component {
         return (
             <div>
               <SideNav />
-               
+              <div className="background-img">
+                    <img src={require("../../Resources/orchard.jpg")}  />
+                    </div>
+              <div className={"cardcolumns"}>
+                    <CardColumns>
+                        <Card>                       
+                            <Card.Body>
+                                <Card.Title>About our farms...</Card.Title>
+                                <Card.Text>
+                                    Our farms are really nice and have nice stuff!
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                       
+                        <Card>                       
+                            <Card.Body>
+                                <Card.Title>About our product...</Card.Title>
+                                <Card.Text>
+                                    Our stuff is better than the next guys, by alot!~
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+  
+                        
+                        </CardColumns>
+
+                    </div>
+                    
+                </div>
+
+        )
+    }
+}
+
+/*Code from pure HTML for reference
+/*                
                 <div className="background-img">
                     <img src={require("../../Resources/orchard.jpg")}  />
                 <h2><span>Our farms are closest to home...</span></h2>
@@ -18,10 +58,7 @@ export class AboutUs extends Component {
                  <h3><span>Info about farms</span></h3>
                  <h4><span>Some more info about the place</span></h4>
                 </div>                   
-                
-            </div>
-        )
-    }
-}
+                 
+</div>*/
 
 export default AboutUs
