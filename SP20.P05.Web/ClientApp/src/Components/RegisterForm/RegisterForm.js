759a9e25ed15a5497484b2c94049929f4b2e5a05
JakeHeld <jakeheld123@gmail.com>
2020-03-01 22:08:06 -0600

    register page

--- SP20.P05.Web/ClientApp/src/Components/RegisterForm/RegisterForm.js
+++ SP20.P05.Web/ClientApp/src/Components/RegisterForm/RegisterForm.js
@@ -1,39 +1,75 @@
 import React, { Component } from 'react'
-import {Button, ButtonToolbar, Content, ControlLabel, FlexboxGrid, Form, FormControl, FormGroup, Panel} from 'rsuite';
+import {Button, Form} from 'react-bootstrap';
+import axios from 'axios';
 
 // CSS
 import './RegisterForm.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export class RegisterForm extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            username:'',
+            password:''
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    url = 'api/customers'
+
+    handleChange(event) {
+        this.setState({ [event.target.name] : event.target.value });
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        let user = JSON.stringify({
+            username: this.state.username,
+            password: this.state.password
+        });
+
+        axios.post(this.url, user, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+                return Promise.resolve(res);
+            })
+    };
+
+
     render() {
         return (
-            <div>
-                <Content id={'registerPadding'}>
-                    <FlexboxGrid justify="center">
-                        <FlexboxGrid.Item colspan={12} >
-                            <Panel header={<h3>Register</h3>} bordered id={'registerBox'}>
-                                <Form fluid>
-                                    <FormGroup>
-                                        <ControlLabel>Username</ControlLabel>
-                                        <FormControl name="username" />
-                                    </FormGroup>
-                                    <FormGroup>
-                                        <ControlLabel>Password</ControlLabel>
-                                        <FormControl name="password" type="password" />
-                                    </FormGroup>
-                                    <FormGroup>
-                                        <ButtonToolbar>
-                                            <Button id={'darkGreenButton'} appearance="primary">Register</Button>
-                                            <Button appearance="secondary" href={'/login'}>Sign In</Button>
-                                        </ButtonToolbar>
-                                    </FormGroup>
-                                </Form>
-                            </Panel>
-                        </FlexboxGrid.Item>
-                    </FlexboxGrid>
-                </Content>
+            <div className={'marginLeft'}>
+                <Form onSubmit={this.handleSubmit}>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control type="username" name = 'username' placeholder="Enter Username"  onChange={this.handleChange} />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" name = 'password' placeholder="Password"  onChange={this.handleChange}/>
+                    </Form.Group>
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Verify Password</Form.Label>
+                        <Form.Control type="password" name = 'verifypassword' placeholder="Verify Password"/>
+                    </Form.Group>
+                    <Button id={'darkGreenButton'} variant="primary" type="submit">
+                        Register
+                    </Button>
+                    <Button id={'registerButton'}  variant="secondary" href={'/login'}>
+                        Login
+                    </Button>
+                </Form>
             </div>
-        )
+        );
     }
 }
 
