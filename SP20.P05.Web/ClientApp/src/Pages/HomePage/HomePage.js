8546b0f53845fe4491563afa5b0fc81db8cc0d7f
David Pedeaux <david.pedeaux@selu.edu>
2020-03-03 10:50:32 -0600

    Set carousel to absolute/added news columns.

--- SP20.P05.Web/ClientApp/src/Pages/HomePage/HomePage.js
+++ SP20.P05.Web/ClientApp/src/Pages/HomePage/HomePage.js
@@ -5,6 +5,7 @@ import SideNav from '../../Components/SideNav/SideNav';
 import Carousel_Home from '../../Components/Carousel/Carousel';
 import Card from 'react-bootstrap/Card';
 import CardColumns from 'react-bootstrap/CardColumns';
+import CardDeck from 'react-bootstrap/CardDeck'
 // CSS
 import './HomePage.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -17,7 +18,37 @@ export class HomePage extends Component {
             <div>
             <SideNav />
             <Carousel_Home/>
+                <CardColumns className="home_carddecks">
+                    <Card>
+                        <Card.Title className="home_cardtitle">
+                            Upcoming Events:
+                        </Card.Title>
+                        <Card.Body className="home_cardbody">
+                            <li>Field Trips</li>
+                            <li>Family Get Togethers</li>
+                            <li>Much More!</li>
+                            
+                        </Card.Body>
+                    </Card>
+                
+                    <Card>
+                        <Card.Title className="home_cardtitle">
+                            News:
+                        </Card.Title>
+                        <Card.Body className="home_cardbody">
+                            Lots of new stuff coming!
+                        </Card.Body>
+                    </Card>
 
+                    <Card>
+                        <Card.Title className="home_cardtitle">
+                            Another Column:
+                        </Card.Title>
+                        <Card.Body className="home_cardbody">
+                            Blah Blah Blah...
+                        </Card.Body>
+                    </Card>
+                </CardColumns>
             </div>
             </body>
         )
