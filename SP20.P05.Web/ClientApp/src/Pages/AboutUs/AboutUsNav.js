595823e33c8ff50b4757d8ad42478a9a7fa4a061
JakeHeld <jakeheld123@gmail.com>
2020-03-03 22:19:10 -0600

    fixed some missed coloring
    login, register are not styled to have brown buttons. need to find a better way to style these navbars other than making new ones every time

--- SP20.P05.Web/ClientApp/src/Pages/AboutUs/AboutUsNav.js
+++ SP20.P05.Web/ClientApp/src/Pages/AboutUs/AboutUsNav.js
@@ -45,20 +45,23 @@ class SideNavbar extends Component {
                     >
                         <Sidenav.Body id="flex-container">
                             <Nav>
-                                <Nav.Item id="sidebar_iconWhite" eventKey="1" icon={<Icon icon="home" />} href='/' >
+                                <Nav.Item id="sidebar_iconGreen" eventKey="1" icon={<Icon icon="home" />} href='/' >
                                     Home
                                 </Nav.Item>
-                                <Nav.Item id="sidebar_iconWhite" eventKey="2" icon={<Icon icon="tree" />} href='/fields'>
+                                <Nav.Item id="sidebar_iconGreen" eventKey="2" icon={<Icon icon="tree" />} href='/fields'>
                                     Browse Fields
                                 </Nav.Item>
-                                <Nav.Item id="sidebar_iconWhite" eventKey="3" icon={<Icon icon="shopping-basket" />}>
+                                <Nav.Item id="sidebar_iconGreen" eventKey="3" icon={<Icon icon="shopping-basket" />}>
                                     Baskets
                                 </Nav.Item>
-                                <Nav.Item id="sidebar_iconWhite" eventKey="4" icon={<Icon icon="order-form" />} href='/tickets'>
+                                <Nav.Item id="sidebar_iconGreen" eventKey="4" icon={<Icon icon="order-form" />} href='/tickets'>
                                     Order Ticket
                                 </Nav.Item>
                                 <Nav.Item id="icon" eventKey="5" icon={<Icon icon="info"/>} href='/about-us'>
                                     About Us
+                                </Nav.Item>
+                                <Nav.Item id="sidebar_iconGreen" eventKey="6" icon= {<Icon icon="user-circle-o"/>} href={'/login'}>
+                                    Login/Register
                                 </Nav.Item>
                             </Nav>
                         </Sidenav.Body>
