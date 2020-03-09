50abd693fa00d73ba055cc7e0aa20367e3b551b9
Jake <jakeheld123@gmail.com>
2020-03-09 17:40:43 -0500

    working on integrating formik to register and making register a function based component

--- SP20.P05.Web/ClientApp/src/Components/RegisterForm/RegisterForm.js
+++ SP20.P05.Web/ClientApp/src/Components/RegisterForm/RegisterForm.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useContext, useState } from "react";
 import { Button } from "rsuite";
 import { Form } from "react-bootstrap";
 import axios from "axios";
@@ -8,100 +8,107 @@ import { Jumbotron } from "react-bootstrap";
 // CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./RegisterForm.css";
+import { Formik } from "formik";
 
-export class RegisterForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: "",
-      password: "",
-      verifypassword: "",
-      registered: ""
-    };
+const  url = "api/customers";
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
+const handleSubmit = async (data, { setSubmitting, resetForm }) => {
+  setSubmitting(true);
 
-  url = "api/customers";
 
-  handleChange(event) {
-    this.setState({ [event.target.name]: event.target.value });
-  }
-
-  handleSubmit = event => {
-    event.preventDefault();
-
-    let user = {
-      username: this.state.username,
-      password: this.state.password
-    };
-
-    if (this.state.password === this.state.verifypassword) {
-      axios
-        .post(this.url, user, {
-          headers: {
-            "Content-Type": "application/json"
-          }
-        })
-        .then(res => {
-          console.log(res);
-          console.log(res.data);
-          if (res.status === 200) this.setState({ registered: true });
-          return Promise.resolve(res);
-        });
-    } else {
-      console.log("Passwords do not match");
-    }
+  let user = {
+    username: data.username,
+    password: data.password,
+    verifypassword: data.verifypassowrd
   };
 
-  render() {
-    if (this.state.registered) return <Redirect to={{ pathname: "/" }} />;
+  if(user.password === user.verifypassword)
+  await axios
+      .post(url, user, {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+
+        if (res.status === 200){
+          return <Redirect to ={{pathname:'/login'}} />
+        }
+
+        return false;
+      });
+
+
 
     return (
       <div className={"centerForm"}>
         <Jumbotron className={"jumbo_clr"}>
           <div className={"formMargins"}>
-            <Form onSubmit={this.handleSubmit}>
-              <Form.Group controlId="formBasicUsername">
-                <Form.Label>Username</Form.Label>
-                <Form.Control
-                  type="username"
-                  name="username"
-                  placeholder="Enter Username"
-                  onChange={this.handleChange}
-                />
-              </Form.Group>
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  name="password"
-                  placeholder="Password"
-                  onChange={this.handleChange}
-                />
-              </Form.Group>
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Verify Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  name="verifypassword"
-                  placeholder="Verify Password"
-                  oncChange={this.handleChange}
-                />
-              </Form.Group>
-              <Button className={"btn_register"} type="submit">
-                Register
-              </Button>
-              <Button variant="secondary">
-                <Link to={"/login"}>Login</Link>
-              </Button>
-            </Form>
+            <Formik
+              initialValues={{ username: "", password: "", verifypassowrd: "" }}
+              onSubmit={handleSubmit}
+            >
+              {({
+                  values,
+                  isSubmitting,
+                  handleChange,
+                  handleBlur,
+                  handleSubmit
+                }) => (
+                  <Form onSubmit={handleSubmit}>
+                    <Form.Group controlId="formBasicUsername">
+                      <Form.Label>Username</Form.Label>
+                      <Form.Control
+                          type="username"
+                          name="username"
+                          placeholder="Enter Username"
+                          value={values.username}
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                      />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicPassword">
+                      <Form.Label>Password</Form.Label>
+                      <Form.Control
+                          type="password"
+                          name="password"
+                          placeholder="Password"
+                          value={values.password}
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                      />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicPassword">
+                      <Form.Label>Verify Password</Form.Label>
+                      <Form.Control
+                          type="password"
+                          name="verifypassword"
+                          placeholder="Verify Password"
+                          value={values.verifypassword}
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                      />
+                    </Form.Group>
+                    <Button
+                        className={"btn_register"}
+                        variant="primary"
+                        disabled={isSubmitting}
+                        type="submit"
+                    >
+                      Register
+                    </Button>
+                    <Button variant="secondary">
+                      <Link to={'/login'}>
+                        Login
+                      </Link>
+                    </Button>
+                  </Form>
+              )}
+            </Formik>
           </div>
         </Jumbotron>
       </div>
     );
   }
-}
-
-export default RegisterForm;
