4a28f0033fe273c8ae4be4c9fe12a7af267a4607
JakeHeld <jakeheld123@gmail.com>
2020-03-12 21:05:09 -0500

    Adding Form Elements

---
+++ SP20.P05.Web/ClientApp/src/Components/TicketForm/TicketForm.js
@@ -0,0 +1,65 @@
+import React from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Form, Col, Row } from "react-bootstrap";
+import {Button} from 'rsuite';
+
+export default function TicketForm() {
+  return (
+    <Form>
+      <Form.Row>
+        <fieldset>
+          <Form.Group controlId="formGridState">
+            <Form.Label>State</Form.Label>
+            <Form.Control as="select" value="Choose...">
+              <option>Choose...</option>
+              <option>Apples</option>
+              <option>Oranges</option>
+              <option>Tangerines</option>
+              <option>PineCones</option>
+              <option>Walnuts</option>
+              <option>Wheat</option>
+              <option>Cacti</option>
+            </Form.Control>
+          </Form.Group>
+
+          <Form.Group as={Row}>
+            <Form.Label as="legend" column sm={2}>
+              Radios
+            </Form.Label>
+            <Col sm={10}>
+              <Form.Check
+                type="radio"
+                label="first radio"
+                name="formHorizontalRadios"
+                id="formHorizontalRadios1"
+              />
+              <Form.Check
+                type="radio"
+                label="second radio"
+                name="formHorizontalRadios"
+                id="formHorizontalRadios2"
+              />
+              <Form.Check
+                type="radio"
+                label="third radio"
+                name="formHorizontalRadios"
+                id="formHorizontalRadios3"
+              />
+            </Col>
+          </Form.Group>
+        </fieldset>
+
+        <Form.Group as={Row}>
+          <Col sm={{ span: 10, offset: 2 }}>
+            <Button type="submit">Sign in</Button>
+          </Col>
+        </Form.Group>
+      </Form.Row>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+}
--- SP20.P05.Web/ClientApp/src/Pages/Tickets/Tickets.js
+++ SP20.P05.Web/ClientApp/src/Pages/Tickets/Tickets.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import TicketForm from '../../Components/TicketForm/TicketForm';
 
 export class Tickets extends Component {
     render() {
         return (
             <div>
-                <h1>Tickets</h1>
+                <TicketForm />
             </div>
         )
     }
