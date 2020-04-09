c7192b317a79a60b9c6658ed35b2d7ae469b2120
DarrylMunsch <darrylmunsch@gmail.com>
2020-04-08 20:37:30 -0500

    Added folder/file structure to mobile

---
+++ SP20-P05-Mobile/StyleSheets.js
@@ -0,0 +1,9 @@
+
+// Use this file for all of our style sheets.
+
+// Example Style w/ export:
+export const navBarStyle = StyleSheet.create({
+
+});
+
+// Import the styles you need for each component, function, etc.. (import { navBarStyle } from './StyleSheets')
