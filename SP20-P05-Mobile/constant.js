a31c5c533dbdc8271e492c10a6591a0e31cccda3
DarrylMunsch <darrylmunsch@gmail.com>
2020-04-08 20:31:12 -0500

    Reset Mobile

--- SP20-P05-Mobile/constant.js
+++
@@ -1,8 +0,0 @@
-export const baseurl = "http://6ee03fa0.ngrok.io";
-
-// Run the below script in the directory under it that I listed.. "the mobile directory7"
-// ngrok http -host-header=rewrite https://localhost:44346/
-// Use the above command in terminal every time you start the app to use an API call under this directory : ../../../Sp20-p05-Mobile
-//It returns a URl you will use.
-//Use that .io url and paste it up here ^
-// and youre good
