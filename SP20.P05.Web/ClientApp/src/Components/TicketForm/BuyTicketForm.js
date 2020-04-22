92e17707609dd7efed80c7c17104d2710fc12757
JakeHeld <jakeheld123@gmail.com>
2020-04-22 00:40:03 -0500

    paypal works again on the web

--- SP20.P05.Web/ClientApp/src/Components/TicketForm/BuyTicketForm.js
+++ SP20.P05.Web/ClientApp/src/Components/TicketForm/BuyTicketForm.js
@@ -1,63 +1,58 @@
-//import React, { Component } from "react";
-//import { Button, Col, Form, Jumbotron } from "react-bootstrap";
-//import Stripe from "./Stripe";
-//import ReactDOM from "react-dom";
-//import { Elements } from "@stripe/react-stripe-js";
-//import { loadStripe } from "@stripe/stripe-js";
-//import CheckoutForm from "./CheckoutForm";
+import React, { Component } from "react";
+import { Button, Col, Form, Jumbotron } from "react-bootstrap";
+import ReactDOM from "react-dom";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
 
 //// CSS
-//import "./TicketForm.css";
-//import "bootstrap/dist/css/bootstrap.min.css";
-//import axios from "axios";
+import "./TicketForm.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import PayPal from "../Paypal/PayPal";
 
-//// Make sure to call `loadStripe` outside of a component’s render to avoid
-//// recreating the `Stripe` object on every render.
-//const stripePromise = loadStripe("pk_test_Rtv1biV3asCHTEeJ0UxXpD6x00P2Q6h9LA");
 
-//class BuyTicketForm extends Component {
-//  constructor(props) {
-//    super(props);
-//  }
 
-//  ticketUrl = "api/tickets";
-//  farmFieldActiveUrl = "api/farm-fields/active";
+class BuyTicketForm extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-//  handleProcessTicket = async data => {
-//    let ticket = {
-//      id: 0,
-//      ticketTimeSlot: "9999-03-23T22:24:13.306Z",
-//      farmFieldId: this.getFarmFieldId
-//    };
+  ticketUrl = "api/tickets";
+  farmFieldActiveUrl = "api/farm-fields/active";
 
-//    await axios
-//      .post(this.ticketUrl, ticket, {
-//        headers: {
-//          "Content-Type": "application/json"
-//        }
-//      })
-//      .then(res => {
-//        console.log(res);
-//        console.log(res.data);
-//      });
-//  };
+  handleProcessTicket = async data => {
+    let ticket = {
+      id: 0,
+      ticketTimeSlot: "9999-03-23T22:24:13.306Z",
+      farmFieldId: this.getFarmFieldId
+    };
+    await axios
+      .post(this.ticketUrl, ticket, {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      });
+  };
 
-//  render() {
-//    return (
-//      <div>
-//        <Jumbotron className={"jumbo_clr"}>
-//          <h1>Purchase Tickets</h1>
-//          <Elements stripe={stripePromise}>
-//            <CheckoutForm />
-//          </Elements>
-//          <div className={"divider div-transparent"} />
-//          <div>
-//            <Button onClick={this.props.changeForm}>Back to Tickets</Button>
-//          </div>
-//        </Jumbotron>
-//      </div>
-//    );
-//  }
-//}
+  render() {
+    return (
+      <div>
+        <Jumbotron className={"jumbo_clr"}>
+          <h1>Purchase Tickets</h1>
+            <PayPal price={sessionStorage.getItem("ticketTotal")} description={'Ticket Purchase from website'} />
+          <div className={"divider div-transparent"} />
+          <div>
+            <Button onClick={this.props.changeForm}>Back to Tickets</Button>
+          </div>
+        </Jumbotron>
+      </div>
+    );
+  }
+}
 
-//export default BuyTicketForm;
+export default BuyTicketForm;
