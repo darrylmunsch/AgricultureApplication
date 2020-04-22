86871bf5a7666d73023ea55dd5eb4e4af07e28de
Jake <jakeheld123@gmail.com>
2020-04-21 20:35:13 -0500

    need to test and pretty up, but should be working

--- SP20-P05-Mobile/Screens/RedeemQrCode.js
+++ SP20-P05-Mobile/Screens/RedeemQrCode.js
@@ -1,29 +1,43 @@
-import { RNCamera } from 'react-native-camera';
-import View from "react-native-web/dist/exports/View";
-import React, {Component} from "react";
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
-class RedeemQrCode extends Component{
+export default function RedeemQrCode() {
+    const [hasPermission, setHasPermission] = useState(null);
+    const [scanned, setScanned] = useState(false);
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>Welcome to React Native!</Text>
-                <Text style={styles.instructions}>To get started, edit App.js</Text>
-                <Text style={styles.instructions}>{instructions}</Text>
-                <RNCamera
-                    ref={ref => {
-                        this.camera = ref;
-                    }}
-                    style={{
-                        flex: 1,
-                        width: '100%',
-                    }}
-                >
-                </RNCamera>
-            </View>
+    useEffect(() => {
+        (async () => {
+            const { status } = await BarCodeScanner.requestPermissionsAsync();
+            setHasPermission(status === 'granted');
+        })();
+    }, []);
 
-        );
+    const handleBarCodeScanned = ({ type, data }) => {
+        setScanned(true);
+        alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    };
+
+    if (hasPermission === null) {
+        return <Text>Requesting for camera permission</Text>;
     }
-}
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
 
-export default RedeemQrCode;
+    return (
+        <View
+            style={{
+                flex: 1,
+                flexDirection: 'column',
+                justifyContent: 'flex-end',
+            }}>
+            <BarCodeScanner
+                onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+                style={StyleSheet.absoluteFillObject}
+            />
+
+            {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+        </View>
+    );
+}
