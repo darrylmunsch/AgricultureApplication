d33543598b69a383143550a03c0e0a83cbca0fa8
DarrylMunsch <darrylmunsch@gmail.com>
2020-04-26 12:22:29 -0500

    rdt

--- SP20-P05-Mobile/Components/CustomerProfile.js
+++ SP20-P05-Mobile/Components/CustomerProfile.js
@@ -1,11 +1,75 @@
 import React, { Component } from "react";
-import { View, Text, ImageBackground, SafeAreaView } from "react-native";
-import { profile } from "../StyleSheets";
+import {
+  View,
+  Text,
+  ImageBackground,
+  SafeAreaView,
+  ActivityIndicator,
+} from "react-native";
+import { ButtonStyle, profile } from "../StyleSheets";
 import { connect } from "react-redux";
+import Axios from "axios";
+import { baseurl } from "../Constants";
+import Button from "../Components/Button";
 
 class CustomerProfile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      tickets: null,
+      error: null,
+    };
+  }
+
+  getTickets = () => {
+    this.setState({ loading: true });
+    const api = Axios.create({
+      baseURL: baseurl,
+    });
+    let id = this.props.auth.user?.id;
+    console.log("id: " + id);
+    api.get(`api/farm-field-tickets/${id}/userTickets`).then((res) => {
+      console.log(res.data);
+      this.setState({
+        tickets: res.data,
+        loading: false,
+      }).catch((err) => {
+        console.log("error catch");
+        this.setState({
+          loading: false,
+          error: err,
+        });
+      });
+    });
+  };
+
   render() {
-    return <Text>customer page</Text>;
+    return (
+      <View>
+        <Text>customer page</Text>
+        {this.state.loading ? (
+          <ActivityIndicator style={{ color: "white", alignSelf: "center" }} />
+        ) : this.state.tickets ? (
+          <View>
+            <Text>Tickets Found</Text>
+            {this.state.error ? (
+              <Text style={{ color: "red" }}>{this.state.error}</Text>
+            ) : null}
+          </View>
+        ) : (
+          <View>
+            <Text>No Tickets Available</Text>
+            <Button
+              label={"Check Tickets"}
+              onPress={this.getTickets}
+              style={ButtonStyle.light}
+              textStyle={ButtonStyle.lightText}
+            />
+          </View>
+        )}
+      </View>
+    );
   }
 }
 function mapStateToProps(state) {
